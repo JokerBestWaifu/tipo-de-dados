0a86a400a24b6a0cd6f9ddb25e38471a4f4b5c98
Theilon Henrique SObral <theilon.sobral@etec.sp.gov.br>
2023-02-14 13:58:24 +0000

    Mudança

--- EstruturaDeRepeticao.java
+++ EstruturaDeRepeticao.java
@@ -1,7 +1,7 @@
 public class EstruturaDeRepeticao {
  public static void main(String[] args) {
     int cc = 0;
-    while (cc<4){
+    while (cc<11){
         System.out.println("teste" + (cc+1));
         cc++;
     }
