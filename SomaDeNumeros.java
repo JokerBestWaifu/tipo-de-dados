fec4b47368896b57e6407e2a2a9d5637a0f8b93c
Theilon Henrique SObral <theilon.sobral@etec.sp.gov.br>
2023-02-27 11:03:21 +0000

    Pronto

--- SomaDeNumeros.java
+++ SomaDeNumeros.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
+
 public class SomaDeNumeros {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        double n1, n2, soma;
 
-        System.out.println("INforme o valor do primeiro numero: ")
+        Scanner input = new Scanner(System.in);
+        int n1, n2, soma;
+
+        System.out.println("INforme o valor do primeiro numero: ");
         n1 =  input.nextInt();
 
-        System.out.println("Informe o valor do segundo numero: ")
+        System.out.println("Informe o valor do segundo numero: ");
         n2 = input.nextInt();
         
         soma = n1 + n2;
 
-        System.out.printf("A Area de um retangulo de base = " + (double)n1);
-        System.out.printf("\n a altura é = " + (double)n2);
-        System.out.printf("\n é = " + (double)soma + "\n");
+        System.out.printf("A Area de um retangulo de base = " + n1);
+        System.out.printf("\n a altura é = " + n2);
+        System.out.printf("\n é = " + soma + "\n");
 
     }
     
